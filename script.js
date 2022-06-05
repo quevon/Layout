41e1793c70c20184450a37171bc1e9f6dbf8dd0a
quevon <quevonmontojo@gmail.com>
2022-06-05 15:00:49 +0800

    added footer

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const main = document.querySelector('#main')
-const sideBar = document.querySelector('.side-bar-left-container')
-
+const main = document.querySelector('#main');
+const sideBar = document.querySelector('.side-bar-left-container');
+const container = document.querySelector('.container');
 
 function hideShowNav() {
-    main.classList.toggle('active')
-    sideBar.classList.toggle('active')
+    main.classList.toggle('active');
+    sideBar.classList.toggle('active');
+    container.classList.toggle('active');
 }
-const toggle = document.querySelector('.toggle')
-toggle.addEventListener('click', hideShowNav)
+const toggle = document.querySelector('.toggle');
+toggle.addEventListener('click', hideShowNav);
